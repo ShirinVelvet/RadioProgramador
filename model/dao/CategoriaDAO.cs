1af0e9ec7ce094d98a28ef28cbceddd3eed49e9f
Fernando <49043927+FernandoPA11@users.noreply.github.com>
2021-12-05 09:04:39 -0600

    dos dao
    Genero y categoria

---
+++ model/dao/CategoriaDAO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using RadioProgramador.model.poco;
+using MySql.Data.MySqlClient;
+using RadioProgramador.tools;
+
+
+namespace RadioProgramador.model.dao {
+    public static class CategoriaDAO {
+
+        public static List<Categoria> GetAll() {
+
+            List<Categoria> categorias = new List<Categoria>();
+            MySqlConnection connection = Database.Conectar();
+
+            try {
+
+				connection.Open();
+
+				string sql = "SELECT id, nombre FROM categorias";
+				MySqlCommand command = new MySqlCommand(sql, connection);
+
+				MySqlDataReader reader = command.ExecuteReader();
+
+				while (reader.Read()) {
+					int id = !reader.IsDBNull(0) ? reader.GetInt32(0) : 0;
+					string nombre = !reader.IsDBNull(1) ? reader.GetString(1) : "";
+					Categoria categoria = new Categoria(id, nombre);
+					categorias.Add(categoria);
+				}
+
+				reader.Close();
+
+			} catch (Exception ex) {
+				// Esto iría al log o algo así
+				Console.WriteLine(ex.Message);
+			}
+
+			connection.Close();
+			return categorias;
+		}
+
+        }
+    }
+}
---
+++ model/dao/GeneroDAO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using RadioProgramador.model.poco;
+using MySql.Data.MySqlClient;
+using RadioProgramador.tools;
+
+namespace RadioProgramador.model.dao {
+    public static class GeneroDAO {
+
+		public static List<Genero> GetAll() {
+
+			List<Genero> generos = new List<Genero>();
+			MySqlConnection connection = Database.Conectar();
+
+			try {
+
+				connection.Open();
+
+				string sql = "SELECT id, nombre FROM generos";
+				MySqlCommand command = new MySqlCommand(sql, connection);
+
+				MySqlDataReader reader = command.ExecuteReader();
+
+				while (reader.Read()) {
+					int id = !reader.IsDBNull(0) ? reader.GetInt32(0) : 0;
+					string nombre = !reader.IsDBNull(1) ? reader.GetString(1) : "";
+					Genero genero = new Genero(id, nombre);
+					generos.Add(genero);
+				}
+
+				reader.Close();
+
+			} catch (Exception ex) {
+				// Esto iría al log o algo así
+				Console.WriteLine(ex.Message);
+			}
+
+			connection.Close();
+			return generos;
+
+		}
+	}
+}
